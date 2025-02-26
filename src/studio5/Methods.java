2c800a252aa8d86cb85a85a06906492e6be2d63e
michellewishner <michellewishner@michelles-mbp.dhcp.wustl.edu>
2025-02-26 14:13:33 -0600

    studio

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -16,6 +16,7 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
+	     distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 		
 		return distance;
 	}
@@ -34,17 +35,21 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		
+		StdDraw.setPenColor(0, 109, 219);  
+		StdDraw.filledCircle(x, y, (3.0 / 4.0) * radius);
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		    StdDraw.setPenColor(146, 0, 0);  
+		    StdDraw.filledCircle(x, y, (1.0 / 2.0) * radius);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		    StdDraw.setPenColor(255, 255, 109);  
+		    StdDraw.filledCircle(x, y, (1.0 / 4.0) * radius);
 		
 	}
 
@@ -60,8 +65,7 @@ public class Methods {
 	 *         characters in the source String with the replacement String
 	 */
 	public static String substituteAll(String source, char target, String replacement) {
-		String result = "";
-		// TODO: Finish this method
+	    StringBuilder result = new StringBuilder();
 		
 		return result;
 	}
@@ -74,8 +78,9 @@ public class Methods {
 	 */
 	public static int arraySum(int[] values) {
 		int sum = 0;
-		// FIXME: Compute the sum of the values in an array
-		
+		for (int i = 0; i<values.length; i++) {
+			sum += values[i];
+		}
 		return sum;
 	}
 
